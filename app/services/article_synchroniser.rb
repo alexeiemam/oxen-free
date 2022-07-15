60ee78c403c0d5eb760c8146e476dc6e83fa7717
Alexei Emam <alexei.emam@upbeatproductions.com>
2022-07-16 00:40:18 +0100

    More backend cruft

--- app/services/article_synchroniser.rb
+++ app/services/article_synchroniser.rb
@@ -11,9 +11,39 @@ class ArticleSynchroniser
   def formatted_articles
     @formatted_articles ||=
       current_articles.map do |api_article|
+        # t.integer :api_id, unique: true, index: true
+        # t.integer :api_collection_id, unique: true, index: true
+        # t.string :api_status, index: true
+        # t.string :api_section, index: true
+        # t.integer :api_view_count, index: true
+        # t.integer :api_like_count, index: true
+        # t.integer :api_impression_count, index: true
+        # t.integer :api_price, index: true
+        # t.integer :api_user_rating_number, index: true
+        # t.integer :api_user_rating_count, index: true
+        # t.integer :api_distance, index: true
+        # t.column :api_location, :point
+        # t.timestamp :api_created_at, index: true
+        # t.timestamp :api_updated_at, index: true
+        # t.timestamp :api_expiry_at, index: true
+
         {
           source: api_article,
-          api_id: api_article['id']
+          api_id: api_article['id'],
+          api_collection_id:  api_article.dig('collection','id'),
+          api_status: api_article['status'],
+          api_section: api_article['section'],
+          api_view_count: api_article.dig('reactions','views'),
+          api_like_count: api_article.dig('reactions','likes'),
+          api_impression_count: api_article.dig('reactions','impressions'),
+          api_price: api_article.dig('value','price'),
+          api_user_rating_number: api_article.dig('user','rating','number'),
+          api_user_rating_count: api_article.dig('value','rating', 'rating'),
+          # api_location: ,
+          api_distance: api_article.dig('location','distance'),
+          api_created_at: Time.parse(api_article['created_at']),
+          api_updated_at: Time.parse(api_article['updated_at']),
+          api_expires_at: Time.parse(api_article['expiry']),
         }
       end
   end
--- db/migrate/20210926221100_create_articles.rb
+++ db/migrate/20210926221100_create_articles.rb
@@ -1,9 +1,23 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
-      t.integer :api_id, unique: true, index: true
-      t.timestamp :published_at, index: true
       t.jsonb :source
+      t.integer :api_id, unique: true, index: true
+      t.integer :api_collection_id, unique: true, index: true
+      t.string :api_status, index: true
+      t.string :api_section, index: true
+      t.integer :api_view_count, index: true
+      t.integer :api_like_count, index: true
+      t.integer :api_impression_count, index: true
+      t.integer :api_price, index: true
+      t.integer :api_user_rating_number, index: true
+      t.integer :api_user_rating_count, index: true
+      t.integer :api_distance, index: true
+      t.column :api_location, :point
+      t.timestamp :api_created_at, index: true
+      t.timestamp :api_updated_at, index: true
+      t.timestamp :api_expires_at, index: true
+      t.timestamp :published_at, index: true
       t.timestamps
     end
   end
