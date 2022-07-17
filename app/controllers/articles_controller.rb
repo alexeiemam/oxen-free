57309791c78c63cbc84b4a74adc9181807673d2c
Alexei Emam <alexei.emam@upbeatproductions.com>
2022-07-17 16:01:35 +0100

    [OXF-001] Display recency information on article list, Add todos for future dev

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,22 @@
 class ArticlesController < ApplicationController
-
   # GET /articles or /articles.json
   def index
+    # @todo
+    # Add Pagination
+
+    # @todo
+    # Add Sorting
+
+    # @todo
+    # Add Filtering
+
+    # @todo
+    # Protect API and app from overzealous fetches due to site popularity
+    # Background record updates only
+    # e.g.
+    # - authenticated postback route from API source to trigger synchronisation
+    # - periodic background fetching by app polling
+    ArticleSynchroniser.new.sync_and_publish! unless params.has_key?(:allow_stale)
     @articles = Article.published
   end
 
