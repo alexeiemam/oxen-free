568be5e078f2ccfeac88d8f7f60fba08335bdd74
Alexei Emam <alexei.emam@upbeatproductions.com>
2022-07-17 16:24:51 +0100

    [OXF-002] Add scaffold for Like resource `rails g scaffold Like`

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :likes
   get 'articles/:api_id', controller: 'articles', action: 'show', :as => :article
 
   root 'articles#index'
