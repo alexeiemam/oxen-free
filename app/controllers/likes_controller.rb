f9834cf7ad0ce9b6d4c97e03eaa0d67fa6721f34
Alexei Emam <alexei.emam@upbeatproductions.com>
2022-07-17 16:31:10 +0100

    [OXF-002] Make likes belong to articles, reduce Like resource endpoints

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,70 +1,13 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: %i[ show edit update destroy ]
+  before_action :set_article
 
-  # GET /likes or /likes.json
-  def index
-    @likes = Like.all
-  end
-
-  # GET /likes/1 or /likes/1.json
-  def show
-  end
-
-  # GET /likes/new
-  def new
-    @like = Like.new
-  end
-
-  # GET /likes/1/edit
-  def edit
-  end
-
-  # POST /likes or /likes.json
   def create
-    @like = Like.new(like_params)
-
-    respond_to do |format|
-      if @like.save
-        format.html { redirect_to like_url(@like), notice: "Like was successfully created." }
-        format.json { render :show, status: :created, location: @like }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /likes/1 or /likes/1.json
-  def update
-    respond_to do |format|
-      if @like.update(like_params)
-        format.html { redirect_to like_url(@like), notice: "Like was successfully updated." }
-        format.json { render :show, status: :ok, location: @like }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /likes/1 or /likes/1.json
-  def destroy
-    @like.destroy
-
-    respond_to do |format|
-      format.html { redirect_to likes_url, notice: "Like was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @article.likes.create!
+    redirect_back_or_to(url_for(@article))
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_like
-      @like = Like.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def like_params
-      params.fetch(:like, {})
+    def set_article
+      @article = Article.from_param(params[:api_id])
     end
 end
--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -13,119 +13,119 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/likes", type: :request do
-  
-  # This should return the minimal set of attributes required to create a valid
-  # Like. As you add validations to Like, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  describe "GET /index" do
-    it "renders a successful response" do
-      Like.create! valid_attributes
-      get likes_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /show" do
-    it "renders a successful response" do
-      like = Like.create! valid_attributes
-      get like_url(like)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_like_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /edit" do
-    it "renders a successful response" do
-      like = Like.create! valid_attributes
-      get edit_like_url(like)
-      expect(response).to be_successful
-    end
-  end
-
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Like" do
-        expect {
-          post likes_url, params: { like: valid_attributes }
-        }.to change(Like, :count).by(1)
-      end
-
-      it "redirects to the created like" do
-        post likes_url, params: { like: valid_attributes }
-        expect(response).to redirect_to(like_url(Like.last))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "does not create a new Like" do
-        expect {
-          post likes_url, params: { like: invalid_attributes }
-        }.to change(Like, :count).by(0)
-      end
-
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post likes_url, params: { like: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested like" do
-        like = Like.create! valid_attributes
-        patch like_url(like), params: { like: new_attributes }
-        like.reload
-        skip("Add assertions for updated state")
-      end
-
-      it "redirects to the like" do
-        like = Like.create! valid_attributes
-        patch like_url(like), params: { like: new_attributes }
-        like.reload
-        expect(response).to redirect_to(like_url(like))
-      end
-    end
-
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        like = Like.create! valid_attributes
-        patch like_url(like), params: { like: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
-  end
-
-  describe "DELETE /destroy" do
-    it "destroys the requested like" do
-      like = Like.create! valid_attributes
-      expect {
-        delete like_url(like)
-      }.to change(Like, :count).by(-1)
-    end
-
-    it "redirects to the likes list" do
-      like = Like.create! valid_attributes
-      delete like_url(like)
-      expect(response).to redirect_to(likes_url)
-    end
-  end
+  # # This should return the minimal set of attributes required to create a valid
+  # # Like. As you add validations to Like, be sure to
+  # # adjust the attributes here as well.
+  # let(:valid_attributes) {
+  #   skip("Add a hash of attributes valid for your model")
+  # }
+  #
+  # let(:invalid_attributes) {
+  #   skip("Add a hash of attributes invalid for your model")
+  # }
+  #
+  # describe "GET /index" do
+  #   it "renders a successful response" do
+  #     Like.create! valid_attributes
+  #     get likes_url
+  #     expect(response).to be_successful
+  #   end
+  # end
+  #
+  # describe "GET /show" do
+  #   it "renders a successful response" do
+  #     like = Like.create! valid_attributes
+  #     get like_url(like)
+  #     expect(response).to be_successful
+  #   end
+  # end
+  #
+  # describe "GET /new" do
+  #   it "renders a successful response" do
+  #     get new_like_url
+  #     expect(response).to be_successful
+  #   end
+  # end
+  #
+  # describe "GET /edit" do
+  #   it "renders a successful response" do
+  #     like = Like.create! valid_attributes
+  #     get edit_like_url(like)
+  #     expect(response).to be_successful
+  #   end
+  # end
+  #
+  # describe "POST /create" do
+  #   context "with valid parameters" do
+  #     it "creates a new Like" do
+  #       expect {
+  #         post likes_url, params: { like: valid_attributes }
+  #       }.to change(Like, :count).by(1)
+  #     end
+  #
+  #     it "redirects to the created like" do
+  #       post likes_url, params: { like: valid_attributes }
+  #       expect(response).to redirect_to(like_url(Like.last))
+  #     end
+  #   end
+  #
+  #   context "with invalid parameters" do
+  #     it "does not create a new Like" do
+  #       expect {
+  #         post likes_url, params: { like: invalid_attributes }
+  #       }.to change(Like, :count).by(0)
+  #     end
+  #
+  #     it "renders a successful response (i.e. to display the 'new' template)" do
+  #       post likes_url, params: { like: invalid_attributes }
+  #       expect(response).to be_successful
+  #     end
+  #   end
+  # end
+  #
+  # describe "PATCH /update" do
+  #   context "with valid parameters" do
+  #     let(:new_attributes) {
+  #       skip("Add a hash of attributes valid for your model")
+  #     }
+  #
+  #     it "updates the requested like" do
+  #       like = Like.create! valid_attributes
+  #       patch like_url(like), params: { like: new_attributes }
+  #       like.reload
+  #       skip("Add assertions for updated state")
+  #     end
+  #
+  #     it "redirects to the like" do
+  #       like = Like.create! valid_attributes
+  #       patch like_url(like), params: { like: new_attributes }
+  #       like.reload
+  #       expect(response).to redirect_to(like_url(like))
+  #     end
+  #   end
+  #
+  #   context "with invalid parameters" do
+  #     it "renders a successful response (i.e. to display the 'edit' template)" do
+  #       like = Like.create! valid_attributes
+  #       patch like_url(like), params: { like: invalid_attributes }
+  #       expect(response).to be_successful
+  #     end
+  #   end
+  # end
+  #
+  # describe "DELETE /destroy" do
+  #   it "destroys the requested like" do
+  #     like = Like.create! valid_attributes
+  #     expect {
+  #       delete like_url(like)
+  #     }.to change(Like, :count).by(-1)
+  #   end
+  #
+  #   it "redirects to the likes list" do
+  #     like = Like.create! valid_attributes
+  #     delete like_url(like)
+  #     expect(response).to redirect_to(likes_url)
+  #   end
+  # end
 end
