b96eda3c473b798c3abe531187cbff23d09cfb42
Alexei Emam <alexei.emam@upbeatproductions.com>
2022-07-17 12:16:37 +0100

    [OXF-001] Correct article collection scopes

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,26 @@
 class Article < ApplicationRecord
 
-  scope :published, -> { where("published_at <= ? AND api_id IS NOT NULL", Time.current) }
-  scope :by_publish_date, -> { order(published_at: :desc, api_id: :desc) }
+  validates_uniqueness_of :api_id
+
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :by_publish_date, -> { order(api_created_at: :desc, api_id: :desc) }
+  scope :by_expiry, -> { order(api_expires_at: :asc) }
+  scope :by_updated, -> { order(api_updated_at: :desc)}
   scope :by_likes, -> { order(api_likes: :desc) }
   scope :by_views, -> { order(api_views: :desc) }
-  scope :by_expiry, -> { order(api_views: :desc) }
-  scope :by_price, -> { order(api_views: :desc) }
-  scope :by_rating, -> { order(api_views: :desc) }
-  scope :by_impressions, -> { order(api_views: :desc) }
+  scope :by_impressions, -> { order(api_impression_count: :desc) }
+  scope :by_price, -> { order(api_views: :api_price) }
+  scope :by_rating, -> { order(api_views: :api_user_rating_number) }
   # scope :by_distance, -> {}
 
-  validates_uniqueness_of :api_id
+ def self.last_checked_at
+   maximum(:published_at)
+ end
+
+ def self.last_updated_at
+   maximum(:api_updated_at)
+ end
+
   def self.from_param(needle)
     find_by(api_id: needle)
   end
